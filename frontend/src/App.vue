856f1bba64c5852de06a7590c581de7c54eba177
Hiran Shah <hiran.shah4@gmail.com>
2023-05-28 21:01:15 +0100

    Made mobile menu component local to AppHeader with navigation drawer component to improve performance & usability

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,53 +2,25 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
 import AppHome from "./components/AppHome.vue";
-import AppFullmenu from "./components/AppFullmenu.vue";
 
 export default {
   name: "App",
   components: {
     AppHeader,
-    AppFullmenu,
     AppFooter,
     AppHome
   },
   data() {
-    return {
-      fullmenuToggle: false,
-      homeComponent: "AppHome"
-    };
+    return {};
   },
-  methods: {
-    showFullmenu() {
-      this.fullmenuToggle = !this.fullmenuToggle;
-    },
-    checkfullscreenMenu() {
-      if (window.innerWidth >= 800) {
-        this.fullmenuToggle = false;
-      }
-    }
-  },
-  mounted() {
-    window.addEventListener("resize", this.checkfullscreenMenu);
-  },
-  watch: {
-    fullmenuToggle() {
-      if (this.fullmenuToggle === true) {
-        this.homeComponent = "AppFullmenu";
-      } else {
-        this.homeComponent = "AppHome";
-      }
-    }
-  }
+  methods: {}
 };
 </script>
 
 <template>
-  <AppHeader @showFullmenu="showFullmenu"></AppHeader>
-  <KeepAlive>
-    <component :is="homeComponent"></component>
-  </KeepAlive>
-  <AppFooter v-show="!fullmenuToggle"></AppFooter>
+  <AppHeader></AppHeader>
+  <AppHome></AppHome>
+  <AppFooter></AppFooter>
 </template>
 
 <style scoped></style>
--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -24,63 +24,102 @@ export default {
     },
     burgerClicked() {
       this.fullscreenMenu = !this.fullscreenMenu;
-      this.$emit("showFullmenu");
+      // this.$emit("showFullmenu");
     }
-  },
-  emits: ["show-fullmenu"]
+  }
 };
 </script>
 
 <template>
   <!-- bg-grey-lighten-1 -->
-  <v-toolbar height="82" class="navbar container" elevation="4">
-    <img class="logo_img" src="../assets/aiwa_logo_nobg.png" />
-    <v-toolbar-title class="logo_text">Ai Wall Art</v-toolbar-title>
+  <v-card>
+    <v-layout>
+      <v-app-bar class="navbar container" elevation="4">
+        <img class="logo_img" src="../assets/aiwa_logo_nobg.png" />
+        <v-app-bar-title class="logo_text">Ai Wall Art</v-app-bar-title>
 
-    <v-responsive max-width="300" v-show="!screenSmall">
-      <v-scroll-x-transition>
-        <v-card-text v-show="searchBar == 1">
-          <v-text-field
-            class="w-5"
-            density="compact"
-            variant="outlined"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-card-text>
-      </v-scroll-x-transition>
-    </v-responsive>
+        <v-card v-show="!screenSmall" style="padding-right: 5vw" elevation="0">
+          <v-responsive max-width="300">
+            <v-scroll-x-transition>
+              <v-card-text v-show="searchBar == 1">
+                <v-text-field
+                  class="w-5"
+                  density="compact"
+                  variant="outlined"
+                  label="Search"
+                  single-line
+                  hide-details
+                ></v-text-field>
+              </v-card-text>
+            </v-scroll-x-transition>
+          </v-responsive>
 
-    <v-btn @click.prevent="searchBar = !searchBar" v-show="!screenSmall" icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+          <v-btn @click.prevent="searchBar = !searchBar" icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
 
-    <v-btn
-      rounded="lg"
-      class="site_font text-none font-weight-bold mr-1 bg-yellow-darken-1"
-      v-show="!screenSmall"
-      >Login</v-btn
-    >
-    <v-btn
-      rounded="lg"
-      class="site_font text-none font-weight-bold ml-1 bg-light-blue-lighten-1"
-      v-show="!screenSmall"
-      >Sign Up</v-btn
-    >
-    <v-btn v-show="!screenSmall" icon>
-      <v-icon>mdi-cart-outline</v-icon>
-    </v-btn>
+          <v-btn rounded="lg" class="site_font text-none font-weight-bold mr-1 bg-yellow-darken-1"
+            >Login</v-btn
+          >
+          <v-btn
+            rounded="lg"
+            class="site_font text-none font-weight-bold ml-1 bg-light-blue-lighten-1"
+            >Sign Up</v-btn
+          >
+          <v-btn icon>
+            <v-icon>mdi-cart-outline</v-icon>
+          </v-btn>
+        </v-card>
 
-    <div v-show="screenSmall">
-      <v-btn v-show="!fullscreenMenu" @click.prevent="burgerClicked" icon>
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-      <v-btn v-show="fullscreenMenu" @click.prevent="burgerClicked" icon>
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-    </div>
-  </v-toolbar>
+        <div v-show="screenSmall" style="padding-right: 4vw">
+          <v-btn v-show="!fullscreenMenu" @click.prevent="burgerClicked" icon>
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+          <v-btn v-show="fullscreenMenu" @click.prevent="burgerClicked" icon>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </div>
+      </v-app-bar>
+
+      <v-navigation-drawer class="w-50" v-model="fullscreenMenu" location="right" temporary>
+        <v-list class="fullmenu d-flex flex-column align-end mr-5">
+          <v-list-item width="300">
+            <v-text-field
+              class="w-5"
+              density="compact"
+              variant="outlined"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-list-item>
+          <v-list-item>
+            <v-btn elevation="0" icon>
+              <v-icon>mdi-cart-outline</v-icon>
+            </v-btn>
+          </v-list-item>
+          <v-list-item>
+            <v-btn
+              rounded="lg"
+              class="site_font text-none font-weight-bold bg-yellow-darken-1"
+              elevation="0"
+              >Login</v-btn
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-btn
+              rounded="lg"
+              class="site_font text-none font-weight-bold bg-light-blue-lighten-1"
+              elevation="0"
+              >Sign Up</v-btn
+            >
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main></v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <style scoped>
@@ -91,5 +130,12 @@ export default {
 }
 .logo_img {
   height: 3rem;
+}
+.fullMenu {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
 }
 </style>
